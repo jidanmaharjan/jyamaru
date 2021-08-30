1708e4b75d70332cf20ebdb0e1671cb245b18fdf
Jidan Maharjan <jidanmaharjan1@gmail.com>
2021-08-31 01:38:36 +0545

    new layout

--- js/navbar.js
+++ js/navbar.js
@@ -5,12 +5,52 @@ function navbarToggle()
 {
     if(toggle == 0)
     {
-        value.style.left = 0;
+        value.style.bottom = "4.5rem";
         toggle = 1;
     }
     else
     {
-        value.style.left = "-100%";
+        value.style.bottom = "-100%";
         toggle = 0;
+    }
+}
+value.onclick = function()
+{
+    value.style.bottom = "-100%";
+        toggle = 0;
+}
+
+
+let dm = document.getElementById("dmtoggler");
+
+
+if(localStorage.getItem("theme")== null)
+{
+    localStorage.setItem("theme","light");
+}
+
+let localData = localStorage.getItem("theme");
+if(localData == "light")
+{
+    dm.src="assets/half-moon.svg";
+    document.body.classList.remove("darkmode");
+}
+else if(localData == "dark")
+{
+    dm.src="assets/sun.svg";
+    document.body.classList.add("darkmode");
+    
+}
+dm.onclick = function(){
+    document.body.classList.toggle("darkmode");
+    if(document.body.classList.contains("darkmode"))
+    {
+        dm.src="assets/sun.svg";
+        localStorage.setItem("theme","dark");
+    }
+    else
+    {
+        dm.src="assets/half-moon.svg";
+        localStorage.setItem("theme","light");
     }
 }
